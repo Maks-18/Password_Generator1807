a17779046d0b4e3073d0bc32364ce9950d88fca5
Maks-18 <m.v.besedin@student.khai.edu>
2025-01-21 23:19:56 +0100

    2-nd stage: add code for password generator

--- src/password_generator/Generator.java
+++ src/password_generator/Generator.java
@@ -1,6 +1,38 @@
-package Generator;
+package password_generator;
+
+import java.util.Scanner;
 
 public class Generator {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int digit = input.nextInt();
+
+        String lower_cases = "qwertyuiopasdfghjklzxcvbnm";
+        String upper_cases = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        String password = "";
+
+        for(int i = 0; i < digit; i++) {
+            int random = (int)(3 * Math.random());
+
+            switch(random){
+                case 0:
+                    password += String.valueOf((int)(0 * Math.random()));
+                    break;
+                case 1:
+                    random = (int)(lower_cases.length() * Math.random());
+                    password += String.valueOf(lower_cases.charAt(random));
+                    break;
+                case 2:
+                    random = (int)(upper_cases.length() * Math.random());
+                    password += String.valueOf(upper_cases.charAt(random));
+                    break;
 
 
+            }
+        }
+
+        System.out.println(password);
+    }
 }
